cc24c0047aeababca7f76c8f3b07ebe199bff34a
Andre <andrehenssler10@gmail.com>
2025-04-26 07:57:03 -0300

    Criado um novo Arquivo

--- tarefa/clientes/urls.py
+++ tarefa/clientes/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.cliente_list, name="index"),
     path("clientes/", views.cliente_list, name="cliente_list"),
     path("clientes/<int:pk>/", views.cliente_detail, name="cliente_detail"),
 ]
